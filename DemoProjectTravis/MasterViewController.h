5696b76fe02ae26af26d8de95bb1d5804374466b
Ashish ojha <ojha.ashish13@gmail.com>
2014-10-10 13:09:13 +0530

    First commit

---
+++ DemoProjectTravis/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  DemoProjectTravis
+//
+//  Created by Ashish ojha on 10/10/14.
+//  Copyright (c) 2014 com.v2solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
