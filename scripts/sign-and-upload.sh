772ac9c6c1bfe516e96ea2b4895fce64670a6332
Ashish ojha <ojha.ashish13@gmail.com>
2014-11-24 11:34:31 +0530

    Updated hockey upload script.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -43,7 +43,7 @@ if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
   echo "***************************"
   echo "* Uploading to Hockeyapp  *"
   echo "***************************"
-  curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload \
+  curl -v https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload \
     -F status="2" \
     -F notify="0" \
     -F notes="$RELEASE_NOTES" \
